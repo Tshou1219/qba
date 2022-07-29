d7248c5d248abf724fc8c31431cee4350d10f024
Tshou1219 <taisuke1219@icloud.com>
2022-07-29 09:33:57 +0900

    add image

--- grover.py
+++ grover.py
@@ -14,16 +14,16 @@ class Grover():
     curved_weight = None
 
     def __init__(self, edges: List[Tuple[int, int]]) -> None:
+        self.initilize_graph(edges)
+
+    def initilize_graph(self, edges):
         self.G = DiGraph(edges+[(b, a) for (a, b) in edges])
+        self.curved_edge = [edge for edge in self.G.edges()]
+        self.curved_weight = [1.0 / len(self.curved_edge)
+                              for _ in range(len(self.curved_edge))]
 
     def run_grover(self, n):
         deg = [int(d/2) for _, d in self.G.degree()]
-        self.curved_edge = [edge for edge in self.G.edges(
-        ) if reversed(edge) in self.G.edges()]
-        self.curved_weight = [0.0 for _ in range(len(self.curved_edge))]
-        # 初期状態の定義
-        self.curved_weight[3] = 1.0
-
         for n in range(n):
             weight_copy = [0.0 for _ in range(len(self.curved_edge))]
             for i, (weight, edges) in enumerate(zip(self.curved_weight, self.curved_edge)):
@@ -44,12 +44,6 @@ class Grover():
         deg = [int(d/2) for _, d in self.G.degree()]
         for p in path:
             deg[p[0]] += 1
-        self.curved_edge = [edge for edge in self.G.edges(
-        ) if reversed(edge) in self.G.edges()]
-        self.curved_weight = [0.0 for _ in range(len(self.curved_edge))]
-        # 初期状態の定義
-        # self.curved_weight[3] = 1.0
-        # self.curved_weight[4] = 1/np.sqrt(2)
 
         grover_matrix = np.full((len(path), len(path)), 2/(len(path)))
         for i in range(len(path)):
@@ -57,7 +51,6 @@ class Grover():
         div = [flowed_weight for flowed_weight in zip(*path)]
         beta_in = div[1]
         beta_out = grover_matrix@beta_in
-        # print(beta_out)
 
         for n in range(n):
             weight_copy = [0.0 for _ in range(len(self.curved_edge))]
@@ -79,13 +72,16 @@ class Grover():
                         else:
                             weight_copy[j] += weight*(2/(deg[edges[0]]))
             # 収束判定
+            if n == 9999 and np.linalg.norm(out-beta_out, ord=2) < 0.01:
+                print(np.linalg.norm(out-beta_out, ord=2))
+                break
             if np.linalg.norm(out-beta_out, ord=2) < 0.01:
                 # print(n)
+                #print(np.linalg.norm(out-beta_out, ord=2))
                 break
             ###
             self.curved_weight = weight_copy
-        # print(out)
-        self.curved_edge_labels = {edge:round(weight,2)  for edge,
+        self.curved_edge_labels = {edge: round(weight, 2) for edge,
                                    weight in zip(self.curved_edge, self.curved_weight)}
 
     def ba_run(self, m, N):
@@ -105,35 +101,31 @@ class Grover():
     def qba_run(self, m, path: List[Tuple[int, float]]):
         nodea = np.array(self.G.nodes())
         weight_array = [0.0 for _ in range(self.G.number_of_nodes())]
-        #print(self.curved_edge, self.curved_weight)
         for edges, weight in zip(self.curved_edge, self.curved_weight):
-            #print(edges[1], self.curved_weight[edges[1]])
             weight_array[edges[1]] += weight**2
         for p in path:
-            # print(p[1])
             weight_array[p[0]] += (p[1])**2
-        # print(weight_array)
         new_node = self.G.number_of_nodes()
         self.G.add_nodes_from([new_node])
         for _ in range(m):
             while True:
                 select = int(str(random.choices(
                     nodea, weight_array/np.sum(weight_array)))[1:].rstrip(']'))
-                # print(select)
                 if self.G.has_edge(new_node, select) or new_node == select:
                     continue
                 self.G.add_edges_from([(new_node, select)])
                 self.G.add_edges_from([(select, new_node)])
                 break
+            self.curved_edge.append((new_node, select))
+            self.curved_edge.append((select, new_node))
+            self.curved_weight.append(0.0)
+            self.curved_weight.append(0.0)
 
-    def complete_graph(self, n: int) -> DiGraph:
+    def complete_graph(self, n: int):
         node = list(map(int, range(n)))
-        self.G = DiGraph()
-        self.G.add_nodes_from(node)
-        self.G.add_edges_from(
-            [(a, b) for idx, a in enumerate(node) for b in node[idx + 1:]])
-        self.G.add_edges_from(
-            [(b, a) for idx, a in enumerate(node) for b in node[idx + 1:]])
+        edges = [(a, b) for idx, a in enumerate(node) for b in node[idx + 1:]] + \
+            [(b, a) for idx, a in enumerate(node) for b in node[idx + 1:]]
+        self.initilize_graph(edges)
 
     def plot(self):
         nx.draw(self.G.to_undirected(), with_labels=True)
@@ -148,7 +140,6 @@ class Grover():
             self.G, pos, ax=ax, edgelist=self.curved_edge, connectionstyle=f'arc3, rad = 0.25')
         label.my_draw_networkx_edge_labels(
             self.G, pos, ax=ax, edge_labels=self.curved_edge_labels, rotate=False, rad=0.25)
-        #print(self.curved_edge, self.curved_weight)
 
     def deg_plot(self):
         # bins = range(1, self.G.number_of_nodes())
@@ -163,7 +154,7 @@ class Grover():
         dmax = max(degree_sequence)
 
         fig = plt.figure("Degree of a random graph", figsize=(8, 8))
-        # Create a gridspec for adding subplots of different sizes
+
         axgrid = fig.add_gridspec(5, 4)
 
         ax0 = fig.add_subplot(axgrid[0:3, :])
