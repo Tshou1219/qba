4dc98dd9fc5e44a3717689118033516cc76177a1
Tshou1219 <taisuke1219@icloud.com>
2022-07-31 10:23:15 +0900

    use neighbors

--- grover.py
+++ grover.py
@@ -12,6 +12,7 @@ class Grover():
     curved_edge = None
     curved_edge_labels = None
     curved_weight = None
+    neighbor = None
 
     def __init__(self, edges: List[Tuple[int, int]]) -> None:
         self.initilize_graph(edges)
@@ -21,6 +22,8 @@ class Grover():
         self.curved_edge = [edge for edge in self.G.edges()]
         self.curved_weight = [1.0 / len(self.curved_edge)
                               for _ in range(len(self.curved_edge))]
+        self.neighbor = [[i for i in self.G.neighbors(
+            j)] for j in range(self.G.number_of_nodes())]
 
     def run_grover(self, n):
         deg = [int(d/2) for _, d in self.G.degree()]
@@ -29,13 +32,15 @@ class Grover():
             for i, (weight, edges) in enumerate(zip(self.curved_weight, self.curved_edge)):
                 if weight == 0:
                     continue
-                for j, edges in enumerate(self.curved_edge):
-                    if edges[0] == self.curved_edge[i][1]:
-                        if edges[1] == self.curved_edge[i][0]:
-                            weight_copy[j] += weight * \
-                                ((2/(deg[edges[0]]))-1)
-                        else:
-                            weight_copy[j] += weight*(2/(deg[edges[0]]))
+                neighbor = [n for n in self.G.neighbors(edges[1])]
+                for neighborhood in neighbor:
+                    j = self.curved_edge.index((edges[1], neighborhood))
+                    if edges[0] == neighborhood:
+                        weight_copy[j] += weight * \
+                            ((2/(deg[edges[1]]))-1)
+                    else:
+                        weight_copy[j] += weight * \
+                            (2/(deg[edges[1]]))
             self.curved_weight = weight_copy
         self.curved_edge_labels = {edge: weight for edge,
                                    weight in zip(self.curved_edge, self.curved_weight)}
@@ -51,7 +56,6 @@ class Grover():
         div = [flowed_weight for flowed_weight in zip(*path)]
         beta_in = div[1]
         beta_out = grover_matrix@beta_in
-
         for n in range(n):
             weight_copy = [0.0 for _ in range(len(self.curved_edge))]
             out = [p[1]*(2/(deg[p[0]])-1) for p in path]
@@ -64,19 +68,23 @@ class Grover():
                 for p in list(div[0]):
                     if self.curved_edge[i][1] == p:
                         out[list(div[0]).index(p)] += weight*(2/(deg[p]))
-                for j, edges in enumerate(self.curved_edge):
-                    if edges[0] == self.curved_edge[i][1]:
-                        if edges[1] == self.curved_edge[i][0]:
-                            weight_copy[j] += weight * \
-                                ((2/(deg[edges[0]]))-1)
-                        else:
-                            weight_copy[j] += weight*(2/(deg[edges[0]]))
+
+                for neighborhood in self.neighbor[edges[1]]:
+                    j = self.curved_edge.index((edges[1], neighborhood))
+                    if edges[0] == neighborhood:
+                        weight_copy[j] += weight * \
+                            ((2/(deg[edges[1]]))-1)
+                    else:
+                        weight_copy[j] += weight * \
+                            (2/(deg[edges[1]]))
+
             # 収束判定
             if n == 9999 and np.linalg.norm(out-beta_out, ord=2) < 0.01:
                 print(np.linalg.norm(out-beta_out, ord=2))
                 break
-            if np.linalg.norm(out-beta_out, ord=2) < 0.01:
-                # print(n)
+            if np.linalg.norm(out-beta_out, ord=2) < 0.0001:
+                # if n != 0:
+                #     print(n, self.G.number_of_nodes()-3)
                 #print(np.linalg.norm(out-beta_out, ord=2))
                 break
             ###
@@ -107,19 +115,22 @@ class Grover():
             weight_array[p[0]] += (p[1])**2
         new_node = self.G.number_of_nodes()
         self.G.add_nodes_from([new_node])
+        self.neighbor.append([])
         for _ in range(m):
             while True:
-                select = int(str(random.choices(
+                selected = int(str(random.choices(
                     nodea, weight_array/np.sum(weight_array)))[1:].rstrip(']'))
-                if self.G.has_edge(new_node, select) or new_node == select:
+                if self.G.has_edge(new_node, selected) or new_node == selected:
                     continue
-                self.G.add_edges_from([(new_node, select)])
-                self.G.add_edges_from([(select, new_node)])
+                self.G.add_edges_from([(new_node, selected)])
+                self.G.add_edges_from([(selected, new_node)])
                 break
-            self.curved_edge.append((new_node, select))
-            self.curved_edge.append((select, new_node))
-            self.curved_weight.append(0.0)
-            self.curved_weight.append(0.0)
+            self.curved_edge.append((new_node, selected))
+            self.curved_weight.append(self.curved_weight[-1])
+            self.curved_edge.append((selected, new_node))
+            self.curved_weight.append(self.curved_weight[-1])
+            self.neighbor[new_node].append(selected)
+            self.neighbor[selected].append(new_node)
 
     def complete_graph(self, n: int):
         node = list(map(int, range(n)))
@@ -142,13 +153,6 @@ class Grover():
             self.G, pos, ax=ax, edge_labels=self.curved_edge_labels, rotate=False, rad=0.25)
 
     def deg_plot(self):
-        # bins = range(1, self.G.number_of_nodes())
-        # dega = np.array(self.G.degree())[:, 1]/2
-        # print(dega)
-        # plt.hist(dega, bins=bins)
-        # plt.show()
-        #G = nx.gnp_random_graph(100, 0.02, seed=10374196)
-        #G = nx.barabasi_albert_graph(100, 2)
         degree_sequence = sorted(
             (d for n, d in self.G.to_undirected().degree()), reverse=True)
         dmax = max(degree_sequence)
