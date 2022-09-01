fb5793903e3c7590d35bef658fee9434761013f9
Tshou1219 <taisuke1219@icloud.com>
2022-09-01 11:24:56 +0900

    make_writer_and_graph_generator

---
+++ write.py
@@ -0,0 +1,24 @@
+import csv
+import os
+import shutil
+import numpy as np
+from qba import QBA
+from networkx import DiGraph
+from typing import List, Tuple
+import matplotlib.pyplot as plt
+import plot
+
+
+def write_data(qba: QBA, path: List[Tuple[int, float]]):
+    dir = './test/'+qba.initial_graph+'/' + str(len(qba.deg))+'_'+str(path)
+    if os.path.isdir(dir):
+        shutil.rmtree(dir)
+    os.makedirs(dir)
+    with open(dir+'/weight.csv', 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow([len(qba.deg)])
+        writer.writerow(path)
+        writer.writerow(qba.weights)
+    plot.count_histgram(qba.count, True, dir+'/count.png')
+    plot.deg_plot(DiGraph(qba.arcs), True, dir+'/deg.png')
+    plot.plot_time(qba.times, True, dir+'/time.png')
